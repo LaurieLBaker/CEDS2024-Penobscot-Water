29e2ee2ff232bd0db1be7ed416f4685ea57f9e79
cedartree03 <ccallahan25@coa.edu>
2024-06-21 14:01:41 -0400

    Reactive filters work!

--- field_report_shiny_app/field_report/app.R
+++ field_report_shiny_app/field_report/app.R
@@ -27,36 +27,34 @@ library(DT)
 data2018_primary <- data2018 %>%
   mutate(Collectors = str_sub(Collectors, start = 1, end = 3))
 
-collectors <- unique(data2018_primary$Collectors)
-dates <- unique(data2018_primary$RunDate)
-runcodes <- unique(data2018_primary$RunCode)
-sitecodes <- unique(data2018_primary$SiteCode)
-
 ui <- page_sidebar(
   sidebar = sidebar(
-    selectInput("collector", label = "Select a Collector", choices = collectors),
-    selectInput("rundate", label = "Select a Date", choices = dates),
-    selectInput("runcode", label = "Select a Run Code", choices = runcodes),
-    selectInput("sitecode", label = "Select a Site Code", choices = sitecodes)
+    selectInput("collector", label = "Select a Collector", choices = data2018_primary$Collectors),
+    selectInput("rundate", label = "Select a Date", NULL),
+    selectInput("runcode", label = "Select a Run", NULL),
+    selectInput("sitecode", label = "Select a Site", NULL)
+    # selectInput("rundate", label = "Select a Date", choices = sort(unique(data2018_primary$RunDate))),
+    # selectInput("runcode", label = "Select a Run Code", choices = data2018_primary$RunCode),
+    # selectInput("sitecode", label = "Select a Site Code", choices = data2018_primary$SiteCode)
   ),
   dataTableOutput("table")
 )
 
 server <- function(input, output, session) {
-  collector <- reactive({
+  collectors <- reactive({
     data2018_primary %>%
       filter(Collectors == input$collector)
   })
-  bindEvent(collectors(), {
+  observeEvent(collectors(), {
     rundate_choice <- unique(collectors()$RunDate)
-    updateSelectInput(session = session, "Run Date", choices = choices)
+    updateSelectInput(session = session, "rundate", choices = rundate_choice)
   })
   rundate <- reactive({
-    filter(collector(), RunDate == input$rundate)
+    filter(collectors(), RunDate == input$rundate)
   })
-  bindEvent(rundate(), {
+  observeEvent(rundate(), {
     runcode_choice <- unique(rundate()$RunCode)
-    updateSelectInput(session = session, "Run ", choices = choices)
+    updateSelectInput(session = session, "runcode", choices = runcode_choice)
   })
   
   output$table <- renderDataTable({
